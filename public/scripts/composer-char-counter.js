f19484926d4ca22a9e4e17748b09583c74b48cd9
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-06-20 14:26:37 -0400

    implement character count - changes when use inputs and color changes when less than 0 chars remaining

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,27 @@
+const updateCharacterCount = function(count) {
+  $('.counter').text(count)
+}
+
+const changeTextColor = function(element, color) {
+  element.css('color', color)
+}
+
+//updates the counter value as user types
+//changes color if numOfChars exceeds 140;
+$(document).ready(function() {
+  $('#tweet-text').on('input', function(e) {
+    let charCounter = 140;
+
+    const innerText = $(this).val();
+    const innerNumOfChars = innerText.length;
+
+    charCounter -= innerNumOfChars;
+    
+    updateCharacterCount(charCounter);
+    if(charCounter < 0) {
+      changeTextColor($('.counter'), '#943B3B');
+    } else {
+      changeTextColor($('.counter'), '#757165');
+    }
+  })
+});
